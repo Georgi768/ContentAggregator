bad5cd16870323ba6bba436ecd43dbd5744b3341
Georgi768 <59054341+Georgi768@users.noreply.github.com>
2021-04-16 13:32:14 +0200

    added global templates and routes

--- aggregator/content/models.py
+++ aggregator/content/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Content(models.Model):
+    Title = models.CharField(max_length=255)
+    Link = models.CharField(max_length=255)
+    Image = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'contents'
